8f954d947185ae9b15b3ce320e697a8f392f5c33
Rajendra Patel <rajendrapatelsmtlabs@gmail.com>
2023-12-19 15:44:31 +0530

    Initial Commit

---
+++ SearchBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchBar
+//
+//  Created by Arti on 19/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
