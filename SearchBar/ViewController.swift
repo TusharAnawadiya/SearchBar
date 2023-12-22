447a0fcfacd514a7602218ceb4f1d258624769e0
TusharAnawadiya <tusharanawadiya444@gmail.com>
2023-12-22 15:41:05 +0530

    Xcode Project Updated.

--- SearchBar/ViewController.swift
+++ SearchBar/ViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDataSource,UISearchBarDelegate{
+    
+    var array:[String] = ["Tushar","Suraj","Madhav","Sumit"]
+    var filters:[String]!
+    
+    @IBOutlet weak var searchbar: UISearchBar!
+    @IBOutlet weak var searchTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        filters = array
+        searchTableView.dataSource = self
+        searchbar.delegate = self
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filters.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = filters[indexPath.row]
+        return cell
+    }
+    
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        print(searchbar.searchTextField.text)
+        filters = []
+        if searchText == ""  {
+            filters == array
+        }
+        for work in array {
+            if work.uppercased().contains(searchText.uppercased()) {
+                filters.append(work)
+            }
+        }
+        self.searchTableView.reloadData()
+    }
+    
+    
 }
 
